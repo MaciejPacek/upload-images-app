e2e84116fb0187ae1a8488443ac19aa06625c716
maciej pacek <maciekpacek1993@gmail.com>
2024-08-01 22:51:47 +0200

    Added first header and main background image

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
+<style lang="less" scoped>
+  @import "./components/main.less";
+</style>  
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App 11"/>
+  <div id="app" style="background: #fffbf8;">
+    <div class="page-background">
+      <div class="main-page">
+        <div class="header-background">
+          <div class="header">
+            <ul class="links">
+              <li class="default-link">
+                <a href="/pl" >
+                  <p class="default-text-main-page text">Home</p>
+                </a>
+              </li>
+              <li class="default-link">
+                <a href="/pl" >
+                  <p class="default-text-main-page text">Galeria</p>
+                </a>
+              </li>
+            </ul>
+            <div class="logo">
+              <img class="img-logo" src="./assets/img/slonecznik.png" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="main-background"></div>
+    <UploadImageComponent />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UploadImageComponent from './components/upload-images-component/UploadImageComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UploadImageComponent
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/upload-images-component/UploadImageComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+     <p>Upload image component</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'UploadImageComponent',
+    data() {
+      return {
+        isOpen: false,  // Definicja zmiennej isOpen w data
+      };
+    },
+    methods: {
+      toggleMenu() {
+        this.isOpen = !this.isOpen;  // Przełączanie stanu isOpen
+      },
+    },
+  };
+  </script>
