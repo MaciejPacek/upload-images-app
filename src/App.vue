554847646dafafab19386dfa807706d606bc95f4
maciej pacek <maciekpacek1993@gmail.com>
2024-08-03 17:22:13 +0200

    Added footer and changed logo img

--- src/App.vue
+++ src/App.vue
@@ -18,16 +18,52 @@
                   <p class="default-text-main-page text">Galeria</p>
                 </a>
               </li>
+              <li class="default-link">
+                <a href="/pl" >
+                  <p class="default-text-main-page text">O nas</p>
+                </a>
+              </li>
             </ul>
             <div class="logo">
-              <img class="img-logo" src="./assets/img/slonecznik.png" />
+              <img class="img-logo" src="./assets/img/slonecznik-log.png" />
             </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="main-background"></div>
+    <div class="main-background">
+      <div class="main">
+        <div class="main-text">
+          <h1 class="big-h2 default-h2"><b>Witamy Was serdecznie na naszym weselu!</b></h1>
+          <div class="text">
+            <p>Jeśli macie ochotę podzielić się z nami swoimi zdjęciami, które zrobiliście podczas wesela to możecie użyć poniższego formularza. </p>
+            <p>Później pośmiejemy się razem :). </p>
+            <p>Jeśli wolicie wysłać zdjęcia w tradycyjny sposób to udostępniliśmy dla Was adres email: <b>maciej.i.kornelia@gmail.com</b></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="slogan-background">
+      <div class="slogan-border">
+        <div class="slogan">
+          
+        </div>
+      </div>
+    </div>
     <UploadImageComponent />
+    <div class="background-footer">
+      <footer>
+        <div class="left">
+          <a href="/pl" class="router-link-active router-link-exact-active">
+            <img class="img-logo" src="./assets/img/slonecznik-log.png" />
+          </a>
+        </div>
+        <div class="right"></div>
+        <div class="copyright">
+          <p>&copy; 2024 Twoja Firma. Wszelkie prawa zastrzeżone.</p>
+        </div>
+      </footer>
+    </div>
   </div>
 </template>
 
